e6ee9bbe01703d0b17975b6123bb0cd17d32caff
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-06-01 21:56:00 +0300

    Create boilerplate initialization

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 require_once "vendor/autoload.php";
+
+$application = new Application();
+
+$start = new class extends Command {
+    protected static $defaultName = "start";
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        return Command::SUCCESS;
+    }
+};
+
+$application->add($start);
+$application->setDefaultCommand("start");
+$application->run();
