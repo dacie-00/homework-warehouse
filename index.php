2b0baa7589c0a22ce6c62d3b5772a4db5ce5038f
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-06-02 18:26:41 +0300

    Generalize saving and loading functions

--- index.php
+++ index.php
@@ -17,27 +17,29 @@ $application = new Application();
 $start = new class extends Command {
     protected static $defaultName = "start";
 
-    private function save(ProductCollection $products): void
+    private function load(string $fileName): ?stdClass
     {
-        $products = json_encode($products, JSON_PRETTY_PRINT);
-        if (!is_dir(__DIR__ . "/db")) {
-            mkdir(__DIR__ . "/db");
-        }
-        file_put_contents(__DIR__ . "/db/db.json", $products);
-    }
-
-    private function load(): ?stdClass
-    {
-        if (file_exists(__DIR__ . "/db/db.json")) {
-            return json_decode(file_get_contents(__DIR__ . "/db/db.json"));
+        if (file_exists(__DIR__ . "/db/$fileName.json")) {
+            return json_decode(file_get_contents(__DIR__ . "/db/$fileName.json"));
         }
         return null;
     }
 
+    private function save(JsonSerializable $serializable, string $fileName): void
+    {
+        $serializable = json_encode($serializable, JSON_PRETTY_PRINT);
+        if (!is_dir(__DIR__ . "/db")) {
+            mkdir(__DIR__ . "/db");
+        }
+        file_put_contents(__DIR__ . "/db/$fileName.json", $serializable);
+    }
+
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $ask = new Ask($input, $output);
-        $warehouse = new ProductCollection($this->load());
+        $warehouse = new ProductCollection($this->load("products"));
+
         while (true) {
             foreach ($warehouse->getAll() as $item) {
                 echo "{$item->getName()} - {$item->getQuantity()}\n";
@@ -47,26 +49,26 @@ $start = new class extends Command {
                 case Ask::ADD_NEW_PRODUCT:
                     [$name, $quantity] = $ask->productInfo();
                     $warehouse->add(new Product(Uuid::uuid4()->toString(), $name, $quantity));
-                    $this->save($warehouse);
+                    $this->save($warehouse, "products");
                     break;
                 case Ask::DELETE_PRODUCT:
                     $id = $ask->product($warehouse->getAll());
                     $warehouse->delete($warehouse->get($id));
-                    $this->save($warehouse);
+                    $this->save($warehouse, "products");
                     break;
                 case ASK::ADD_PRODUCT:
                     $id = $ask->product($warehouse->getAll());
                     $product = $warehouse->get($id);
                     $quantity = $ask->quantity(0);
                     $product->setQuantity($product->getQuantity() + $quantity);
-                    $this->save($warehouse);
+                    $this->save($warehouse, "products");
                     break;
                 case ASK::WITHDRAW_PRODUCT:
                     $id = $ask->product($warehouse->getAll());
                     $product = $warehouse->get($id);
                     $quantity = $ask->quantity(1, $product->getQuantity());
                     $product->setQuantity($product->getQuantity() + $quantity);
-                    $this->save($warehouse);
+                    $this->save($warehouse, "products");
                     break;
                 case Ask::EXIT:
                     return Command::SUCCESS;
