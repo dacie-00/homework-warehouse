654790a3f4647df7856419243d21fe51e4770af6
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-06-02 14:44:30 +0300

    Start work on basic execution loop

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+use App\Ask;
+use App\Product;
+use App\ProductCollection;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,6 +19,26 @@ $start = new class extends Command {
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $ask = new Ask($input, $output);
+        $warehouse = new ProductCollection();
+        while (true) {
+            $mainAction = $ask->mainAction();
+            switch ($mainAction) {
+                case Ask::ADD_PRODUCT:
+                    [$name, $quantity] = $ask->productInfo();
+                    $warehouse->add(new Product(Uuid::uuid4()->toString(), $name, $quantity));
+                    break;
+                case Ask::DELETE_PRODUCT:
+                    $productName = $ask->product($warehouse->getAll());
+                    echo $productName;
+                    break;
+                case Ask::EXIT:
+                    exit("Bye!");
+            }
+            foreach ($warehouse->getAll() as $item) {
+                echo "{$item->getName()} - {$item->getQuantity()}\n";
+            }
+        }
 
         return Command::SUCCESS;
     }
